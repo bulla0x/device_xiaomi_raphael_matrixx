39ae4d85f92de9c3dd570b7cad145b709000a101
bulla03x <jyotishmanbaruah67@gmail.com>
2025-05-20 21:21:18 +0530

    raphael: Drop COMMON_LUNCH_CHOICES

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -6,8 +6,3 @@
 
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/pixelage_raphael.mk
-
-COMMON_LUNCH_CHOICES := \
-    pixelage_raphael-ap3a-user \
-    pixelage_raphael-ap3a-userdebug \
-    pixelage_raphael-ap3a-eng
