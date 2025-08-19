5ab5da863cfd799d7a09a4e75e1f5401ff3707bc
bulla03x <jyotishmanbaruah67@gmail.com>
2025-08-19 23:22:56 +0530

    raphael: adopt to lineage

--- lineage_raphael.mk
+++ lineage_raphael.mk
@@ -15,8 +15,8 @@ TARGET_SUPPORTS_OMX_SERVICE := false
 # Inherit from raphael device
 $(call inherit-product, device/xiaomi/raphael/device.mk)
 
-# Inherit some common YAAP stuff.
-$(call inherit-product, vendor/yaap/config/common_full_phone.mk)
+# Inherit some common LINEAGE stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Gapps
 #TARGET_BUILD_GAPPS := true
@@ -30,7 +30,7 @@ TARGET_SCREEN_WIDTH := 1080
 TARGET_PROVIDES_POWERHAL := true
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := yaap_raphael
+PRODUCT_NAME := lineage_raphael
 PRODUCT_DEVICE := raphael
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MANUFACTURER := Xiaomi
